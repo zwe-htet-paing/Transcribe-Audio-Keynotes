703003e6326fda1e2797af2cabf449501442e07c
Aungkokolin1997 <lin@quartile.co>
2024-04-13 15:47:35 +0700

    [ADD] excel export feature

--- demo.py
+++ demo.py
@@ -1,14 +1,14 @@
-{
-  "Audio": "/hom/kasdfadf/test.mp4",
-  "Language": "english",
-  "speaker": ["speaker_1", "speaker_2"],
-  "Timestamp": ["2021-09-01 12:00:00", "2021-09-01 12:00:10"],
-  "Transcript": ["Hello, how are you doing today?"],
-}
-
-
-{
-  "Audio": "/hom/kasdfadf/test.mp4",
-  "Language": "english",
-  "KeyNotes": "Hello, how are you doing today?",
-}
+data = {
+        "Audio": "/hom/kasdfadf/test.mp4",
+        "Language": "english",
+        "speaker": ["speaker_1", "speaker_2"],
+        "Timestamp": ["2021-09-01 12:00:00", "2021-09-01 12:00:10"],
+        "Transcript": ["Hello, how are you doing today?", "I am fine."],
+        "task": "Transcribe"
+    }
+keynote_data = {
+        "Audio": "/hom/kasdfadf/test.mp4",
+        "Language": "english",
+        "KeyNotes": "Hello, how are you doing today?",
+        "task": "Note",
+    }
